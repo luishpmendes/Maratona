f5492ef928fa9b0034b20b932137ebac524f06d0
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-07-16 16:31:03 -0300

    dfs

--- lib/graph/matrix/dfs/dfs.cpp
+++ lib/graph/matrix/dfs/dfs.cpp
@@ -1,16 +1,12 @@
-#include <algorithm>
-#include <cstdlib>
-#include <climits>
-#include <cstring>
-#include <list>
-#include <queue>
+#include <iostream>
+#include <vector>
 
 #ifndef INFINITE
-#define INFINITE INT_MAX
+#define INFINITE 15 << 25
 #endif
 
 #ifndef NIL
-#define NIL INT_MIN
+#define NIL - 15 << 25
 #endif
 
 #ifndef WHITE
@@ -29,52 +25,66 @@ using namespace std;
 
 typedef vector< vector<int> > matrix;
 
-void dfsVisit(matrix W, int n, int u, int * time, int color[], int pi[], int d[], int f[]) {
-    if (time != NULL) {
-        (*time)++;
-        if (d != NULL) {
-            d[u] = (*time);
-        }
-    }
+void dfsVisit(matrix W, int u, int * time, vector<int> * color, vector<int> * pi, vector<int> * d, vector<int> * f) {
+    (*time)++;
+    (*d)[u] = (*time);
 
-    color[u] = GRAY;
+    (*color)[u] = GRAY;
 
-    for (int v = 0; v < n; v++) {
+    for (int v = 0; v < (int) W.size(); v++) {
         if (u != v && W[u][v] < INFINITE) {
-            if (color[v] == WHITE) {
-                if(pi != NULL) {
-                    pi[v] = u;
-                }
-                dfsVisit(W, n, v, time, color, pi, d, f);
+            if ((*color)[v] == WHITE) {
+                (*pi)[v] = u;
+                dfsVisit(W, v, time, color, pi, d, f);
             }
         }
     }
 
-    color[u] = BLACK;
-    if (time != NULL) {
-        (*time)++;
-        if (f != NULL) {
-            f[u] = (*time);
-        }
-    }
+    (*color)[u] = BLACK;
+    (*time)++;
+    (*f)[u] = (*time);
 }
 
-void dfs(matrix W, vector<int> pi, int d[], int f[]) {
-    vector<int> color;
+void dfs(matrix W, vector<int> * pi, vector<int> * d, vector<int> * f) {
+    vector<int> color (W.size(), WHITE);
     int time;
 
-    for(int u = 0; u < n; u++) {
+    (*pi) = vector<int> (W.size(), NIL);
+    (*d) = vector<int> (W.size(), INFINITE);
+    (*f) = vector<int> (W.size(), INFINITE);
+
+    for (int u = 0; u < (int) W.size(); u++) {
         color[u] = WHITE;
-        if(pi != NULL) {
-            pi[u] = NIL;
-        }
+        (*pi)[u] = NIL;
+        (*d)[u] = INFINITE;
+        (*f)[u] = INFINITE;
     }
 
     time = 0;
 
-    for (int u = 0; u < n; u++) {
-        if(color[u] == WHITE) {
-            dfsVisit(W, n, u, &time, color, pi, d, f);
+    for (int u = 0; u < (int) W.size(); u++) {
+        if (color[u] == WHITE) {
+            dfsVisit(W, u, &time, &color, pi, d, f);
         }
     }
+}
+
+int main () {
+    int n, m;
+    while (cin >> n >> m) {
+        matrix W (n, vector<int>(n, INFINITE));
+        vector<int> pi;
+        vector<int> d;
+        vector<int> f;
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            W[u][v] = 1;
+        }
+        dfs(W, &pi, &d, &f);
+        for (int i = 0; i < n; i++) {
+            cout << pi[i] << " " << d[i] << " " << f[i] << endl;
+        }
+    }
+    return 0;
 }
