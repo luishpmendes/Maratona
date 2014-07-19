a3fc69bac19f0ddf3978c771e7819f6802b170a8
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-07-19 16:06:58 -0300

    dfs

---
+++ lib/graph/list/dfs/dfs.cpp
@@ -0,0 +1,93 @@
+#include <list>
+#include <iostream>
+#include <vector>
+
+#ifndef INFINITE
+#define INFINITE 15 << 25
+#endif
+
+#ifndef NIL
+#define NIL - 15 << 25
+#endif
+
+#ifndef WHITE
+#define WHITE 0
+#endif
+
+#ifndef GRAY
+#define GRAY  1
+#endif
+
+#ifndef BLACK
+#define BLACK 2
+#endif
+
+using namespace std;
+
+typedef struct {
+    int v; /* vertex destination */
+    int w; /* edge weight */
+} node;
+
+void dfsVisit(vector< list <node> > adj, int u, int * time, vector<int> * color, vector<int> * pi, vector<int> * d, vector<int> * f) {
+    (*time)++;
+    (*d)[u] = (*time);
+
+    (*color)[u] = GRAY;
+
+    for (list<node>::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
+        int v = (*it).v;
+        if ((*color)[v] == WHITE) {
+            (*pi)[v] = u;
+            dfsVisit(adj, v, time, color, pi, d, f);
+        }
+    }
+
+    (*color)[u] = BLACK;
+    (*time)++;
+    (*f)[u] = (*time);
+}
+
+void dfs(vector< list <node> > adj, vector<int> * pi, vector<int> * d, vector<int> * f) {
+    vector<int> color (adj.size(), WHITE);
+    int time;
+
+    (*pi) = vector<int> (adj.size(), NIL);
+    (*d) = vector<int> (adj.size(), INFINITE);
+    (*f) = vector<int> (adj.size(), INFINITE);
+
+    for (int u = 0; u < (int) adj.size(); u++) {
+        color[u] = WHITE;
+        (*pi)[u] = NIL;
+        (*d)[u] = INFINITE;
+        (*f)[u] = INFINITE;
+    }
+
+    time = 0;
+
+    for (int u = 0; u < (int) adj.size(); u++) {
+        if (color[u] == WHITE) {
+            dfsVisit(adj, u, &time, &color, pi, d, f);
+        }
+    }
+}
+
+int main () {
+    int n, m;
+    while (cin >> n >> m) {
+        vector< list <node> > adj (n);
+        vector<int> pi;
+        vector<int> d;
+        vector<int> f;
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back((node) {v, 1});
+        }
+        dfs(adj, &pi, &d, &f);
+        for (int i = 0; i < n; i++) {
+            cout << pi[i] << " " << d[i] << " " << f[i] << endl;
+        }
+    }
+    return 0;
+}
