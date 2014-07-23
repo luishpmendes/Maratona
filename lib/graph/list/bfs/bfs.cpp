bdca66136ef0933daf79ac5ed4c65ca8d31a1579
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-07-23 16:51:32 -0300

    bfs

--- lib/graph/list/bfs/bfs.cpp
+++ lib/graph/list/bfs/bfs.cpp
@@ -25,9 +25,9 @@
 
 using namespace std;
 
-void bfs (vector< list < pair <int, double> > > adj, int s, vector<int> * pi, vector<int> * d) {
-    vector<int> color (adj.size(), WHITE);
-    queue<int> Q;
+void bfs (vector < list < pair <int, double> > > adj, int s, vector<int> * pi, vector<int> * d) {
+    vector <int> color (adj.size(), WHITE);
+    queue <int> Q;
 
     (*pi) = vector<int> (adj.size(), NIL);
     (*d) = vector<int> (adj.size(), INFINITE);
@@ -48,7 +48,7 @@ void bfs (vector< list < pair <int, double> > > adj, int s, vector<int> * pi, ve
         int u = Q.front();
         Q.pop();
 
-        for (list< pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
+        for (list < pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
             int v = (*it).first;
             if (color[v] == WHITE) {
                 color[v] = GRAY;
@@ -64,10 +64,10 @@ void bfs (vector< list < pair <int, double> > > adj, int s, vector<int> * pi, ve
 int main () {
     int n, m;
     while (cin >> n >> m) {
-        vector< list < pair <int, double> > > adj (n);
+        vector < list < pair <int, double> > > adj (n);
         int s;
-        vector<int> pi;
-        vector<int> d;
+        vector <int> pi;
+        vector <int> d;
         for (int i = 0; i < m; i++) {
             int u, v;
             cin >> u >> v;
@@ -75,7 +75,7 @@ int main () {
             adj[v].push_back(make_pair(u, 1));
         }
         cin >> s;
-        bfs(adj, s, &pi, &d);
+        bfs (adj, s, &pi, &d);
         for (int i = 0; i < n; i++) {
             cout << pi[i] << " " << d[i] << endl;
         }
