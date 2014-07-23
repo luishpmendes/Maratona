569393d6a1cd1b5653b5bc60dcbce9cbf428efe5
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-07-23 16:57:27 -0300

    topological sort

--- lib/graph/list/topologicalSort/topologicalSort.cpp
+++ lib/graph/list/topologicalSort/topologicalSort.cpp
@@ -16,10 +16,10 @@
 
 using namespace std;
 
-void dfsVisit (vector< list < pair <int, double> > > adj, int u, vector<int> * color, list<int> * l) {
+void dfsVisit (vector < list < pair <int, double> > > adj, int u, vector <int> * color, list <int> * l) {
     (*color)[u] = GRAY;
 
-    for (list< pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
+    for (list < pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
         int v = (*it).first;
         if ((*color)[v] == WHITE) {
             dfsVisit(adj, v, color, l);
@@ -30,34 +30,34 @@ void dfsVisit (vector< list < pair <int, double> > > adj, int u, vector<int> * c
     (*l).push_front(u);
 }
 
-list<int> dfs (vector< list < pair <int, double> > > adj) {
-    list<int> result;
-    vector<int> color (adj.size(), WHITE);
+list <int> dfs (vector < list < pair <int, double> > > adj) {
+    list <int> result;
+    vector <int> color (adj.size(), WHITE);
 
     for (int u = 0; u < (int) adj.size(); u++) {
         if (color[u] == WHITE) {
-            dfsVisit(adj, u, &color, &result);
+            dfsVisit (adj, u, &color, &result);
         }
     }
 
     return result;
 }
 
-vector<int> topologicalSort (vector< list < pair <int, double> > > adj) {
-    list<int> l = dfs (adj);
-    return vector<int> (l.begin(), l.end());
+vector <int> topologicalSort (vector < list < pair <int, double> > > adj) {
+    list <int> l = dfs (adj);
+    return vector <int> (l.begin(), l.end());
 }
 
 int main () {
     int n, m;
     while (cin >> n >> m) {
-        vector< list < pair <int, double> > > adj (n);
+        vector < list < pair <int, double> > > adj (n);
         for (int i = 0; i < m; i++) {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(make_pair(v, 1));
         }
-        vector<int> ts = topologicalSort(adj);
+        vector <int> ts = topologicalSort (adj);
         for (int i = 0; i < n; i++) {
             cout << ts[i] << endl;
         }
