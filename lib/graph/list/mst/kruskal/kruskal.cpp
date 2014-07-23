32d60b4fa9212e6b8c7fec5562b5481345caaf02
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-07-23 16:56:16 -0300

    kruskal

--- lib/graph/list/mst/kruskal/kruskal.cpp
+++ lib/graph/list/mst/kruskal/kruskal.cpp
@@ -17,13 +17,13 @@ typedef struct {
 } Edge;
 
 struct Sets {
-    vector<int> p;
-    vector<int> rank;
+    vector <int> p;
+    vector <int> rank;
 };
 
 void SETSinit (Sets * S, int n) {
-    S->p = vector<int> (n, NIL);
-    S->rank = vector<int> (n, NIL);
+    S->p = vector <int> (n, NIL);
+    S->rank = vector <int> (n, NIL);
 
     for (int i = 0; i < n; i++) {
         S->p[i] = NIL;
@@ -69,9 +69,9 @@ void SETSunion (Sets * S, int x, int y) {
     SETSlink(S, SETSfind(S, x), SETSfind(S, y));
 }
 
-vector<Edge> minEdges (vector< list < pair <int, double> > > adj) {
-    vector<Edge> result;
-    map<pair<int, int>, double> M;
+vector <Edge> minEdges (vector < list < pair <int, double> > > adj) {
+    vector <Edge> result;
+    map <pair <int, int>, double> M;
 
     for (int u = 0; u < (int) adj.size(); u++) {
         for (list< pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
@@ -90,7 +90,7 @@ vector<Edge> minEdges (vector< list < pair <int, double> > > adj) {
         }
     }
 
-    for (map<pair<int, int>, double>::iterator it = M.begin(); it != M.end(); ++it) {
+    for (map <pair <int, int>, double>::iterator it = M.begin(); it != M.end(); ++it) {
         Edge e;
         e.u = ((*it).first).first;
         e.v = ((*it).first).second;
@@ -105,32 +105,32 @@ bool comparator (Edge a, Edge b) {
     return (a.w < b.w);
 }
 
-vector<Edge> kruskal (vector< list < pair <int, double> > > adj) {
-    vector<Edge> A; /* A ← ∅ */
+vector <Edge> kruskal (vector < list < pair <int, double> > > adj) {
+    vector <Edge> A; /* A ← ∅ */
     Sets S;
 
-    SETSinit(&S, adj.size());
+    SETSinit (&S, adj.size());
 
     /* for each vertex v ∈ V[G] */
     for (int v = 0; v < (int) adj.size(); v++) {
-        SETSmake(&S, v);
+        SETSmake (&S, v);
     }
 
     /* sort the edges of E into nondecreasing order by weight w */
-    vector<Edge> E = minEdges(adj);
+    vector <Edge> E = minEdges(adj);
 
     sort(E.begin(), E.end(), comparator);
 
     /* for each edge (u, v) ∈ E, taken in nondecreasing order by weight */
-    for (vector<Edge>::iterator it = E.begin(); it != E.end(); ++it) {
+    for (vector <Edge>::iterator it = E.begin(); it != E.end(); ++it) {
         Edge e = *it;
         if (SETSfind(&S, e.u) != SETSfind(&S, e.v)) {
             A.push_back(e);
-            SETSunion(&S, e.u, e.v);
+            SETSunion (&S, e.u, e.v);
         }
     }
 
-    SETSdestroy(&S);
+    SETSdestroy (&S);
 
     return A;
 }
@@ -138,7 +138,7 @@ vector<Edge> kruskal (vector< list < pair <int, double> > > adj) {
 int main () {
     int n, m;
     while (cin >> n >> m) {
-        vector< list <pair <int, double> > > adj (n);
+        vector < list <pair <int, double> > > adj (n);
         for (int i = 0; i < m; i++) {
             int u, v;
             double w;
@@ -146,8 +146,8 @@ int main () {
             adj[u].push_back(make_pair(v, w));
             adj[v].push_back(make_pair(u, w));
         }
-        vector<Edge> A = kruskal(adj);
-        for (vector<Edge>::iterator it = A.begin() ; it != A.end(); ++it) {
+        vector <Edge> A = kruskal(adj);
+        for (vector <Edge>::iterator it = A.begin(); it != A.end(); ++it) {
             Edge e = *it;
             cout << e.u << " " << e.v << " " << e.w << endl;
         }
