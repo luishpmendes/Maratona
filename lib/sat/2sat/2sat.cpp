7cb0de0eab17e2ef3724d8ad2ecae6073236b294
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-08-24 16:21:54 -0300

    2sat

--- lib/sat/2sat/2sat.cpp
+++ lib/sat/2sat/2sat.cpp
@@ -1,112 +1,75 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <stack>
 #include <string>
 #include <vector>
 
-#ifndef INFINITE
-#define INFINITE 15 << 25
-#endif
-
-#ifndef WHITE
-#define WHITE 0
-#endif
-
-#ifndef GRAY
-#define GRAY  1
-#endif
-
-#ifndef BLACK
-#define BLACK 2
+#ifndef NIL
+#define NIL - (15 << 25)
 #endif
 
 using namespace std;
 
-typedef vector < vector <double> > matrix;
-
-void dfsVisit (matrix W, int u, vector <int> * color, list <int> * l) {
-    (*color)[u] = GRAY;
-    for (int v = 0; v < (int) W.size(); v++) {
-        if (u != v && W[u][v] < INFINITE) {
-            if ((*color)[v] == WHITE) {
-                dfsVisit(W, v, color, l);
+void tarjanAux (vector < list < pair <int, double> > > adj, stack <int> * S, vector <bool> * inS, int * i, vector <int> * index, vector <int> * lowLink, int u, vector <int> * scc, int * ncc) {
+    (*index)[u] = *i;
+    (*lowLink)[u] = *i;
+    (*i)++;
+    (*S).push(u);
+    (*inS)[u] = true;
+    for (list < pair <int, double> >::iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
+        int v = (*it).first;
+        if ((*index)[v] < 0) {
+            tarjanAux (adj, S, inS, i, index, lowLink, v, scc, ncc);
+            if ((*lowLink)[u] > (*lowLink)[v]) {
+                (*lowLink)[u] = (*lowLink)[v];
+            }
+        } else if ((*inS)[v]) {
+            if ((*lowLink)[u] > (*lowLink)[v]) {
+                (*lowLink)[u] = (*lowLink)[v];
             }
         }
     }
-    (*color)[u] = BLACK;
-    (*l).push_front(u);
+    if ((*lowLink)[u] == (*index)[u]) {
+        int v = NIL;
+        do {
+            v = (*S).top();
+            (*S).pop();
+            (*inS)[v] = false;
+            (*scc)[v] = *ncc;
+        } while (u != v);
+        (*ncc)++;
+    }
 }
 
-list <int> dfs (matrix W) {
-    list <int> result;
-    vector <int> color (W.size(), WHITE);
-    for (int u = 0; u < (int) W.size(); u++) {
-        if (color[u] == WHITE) {
-            dfsVisit(W, u, &color, &result);
+int tarjan (vector < list < pair <int, double> > > adj, vector <int> * scc) {
+    *scc = vector <int> (adj.size(), NIL);
+    int ncc = 0;
+    stack <int> S;
+    vector <bool> inS (adj.size(), false);
+    int i = 0;
+    vector <int> index (adj.size(), NIL);
+    vector <int> lowLink (adj.size(), NIL);
+    for (int u = 0; u < (int) adj.size(); u++) {
+        if (index[u] < 0) {
+            tarjanAux (adj, &S, &inS, &i, &index, &lowLink, u, scc, &ncc);
         }
     }
-    return result;
-}
-
-vector <int> topologicalSort (matrix W) {
-    list <int> l = dfs (W);
-    return vector <int> (l.begin(), l.end());
-}
-
-matrix transpose (matrix W) {
-    matrix result (W.size(), vector <double> (W.size(), INFINITE));
-    for (int u = 0; u < (int) W.size(); u++) {
-        for (int v = 0; v < (int) W.size(); v++) {
-            result[v][u] = W[u][v];
-        }
-    }
-    return result;
-}
-
-void dfsTVisit (matrix W, int u, vector <int> * color, vector <int> * scc, int ncc) {
-    (*color)[u] = GRAY;
-    for (int v = 0; v < (int) W.size(); v++) {
-        if (u != v && W[u][v] < INFINITE) {
-            if ((*color)[v] == WHITE) {
-                dfsTVisit(W, v, color, scc, ncc);
-            }
-        }
-    }
-    (*color)[u] = BLACK;
-    (*scc)[u] = ncc;
-}
-
-int dfsT (matrix W, vector <int> ts, vector <int> * scc) {
-    int result = 0;
-    *scc = vector <int> (W.size(), -1);
-    vector <int> color (W.size(), WHITE);
-    for (int i = 0; i < (int) ts.size(); i++) {
-        int u = ts[i];
-        if (color[u] == WHITE) {
-            dfsTVisit (W, u, &color, scc, result++);
-        }
-    }
-    return result;
-}
-
-int stronglyConnectedComponents (matrix W, vector <int> * scc) {
-    vector <int> ts = topologicalSort (W);
-    matrix T = transpose (W);
-    return dfsT (T, ts, scc);
+    return ncc;
 }
 
 bool sat (vector < pair < pair <int, bool>, pair <int, bool> > > V, int m) {
-    matrix W (2*m, vector <double> (2*m, INFINITE));
+    vector < list < pair <int, double> > > adj (2*m);
     for (int i = 0; i < (int) V.size(); i++) {
         int id1 = V[i].first.first;
         int b1 = V[i].first.second ? 1 : 0;
         int id2 = V[i].second.first;
         int b2 = V[i].second.second ? 1 : 0;
-        W[(1 - b1) * m + id1][b2 * m + id2] = 1;
-        W[(1 - b2) * m + id2][b1 * m + id1] = 1;
+        adj[(1 - b1) * m + id1].push_back(make_pair(b2 * m + id2, 1));
+        adj[(1 - b2) * m + id2].push_back(make_pair(b1 * m + id1, 1));
     }
     vector <int> scc;
-    stronglyConnectedComponents (W, &scc);
+    tarjan (adj, &scc);
     for (int i = 0; i < m; i++) {
         if (scc[i] == scc[m + i]) {
             return false;
