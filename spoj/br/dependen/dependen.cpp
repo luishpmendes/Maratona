f74ca328e6ff392c481efd5f954819200b197d8a
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-08-29 18:30:56 -0300

    dependen

---
+++ spoj/br/dependen/dependen.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+typedef vector < vector <int> > matrix;
+
+int main () {
+    int n;
+    while (scanf(" %d", &n) == 1 && n > 0) {
+        matrix W (n, vector <int> (n, 0));
+        for (int u = 0; u < n; u++) {
+            int t;
+            for (scanf(" %d", &t); t > 0; t--) {
+                int v;
+                scanf(" %d", &v);
+                v--;
+                W[u][v] = 1;
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    W[i][j] |= W[i][k] & W[k][j];
+                }
+            }
+        }
+        vector <int> dependen (n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dependen[i] += W[i][j];
+            }
+        }
+        int uMax = 0, max = dependen[0];
+        for (int u = 1; u < n; u++) {
+            if (max < dependen[u]) {
+                max = dependen[u];
+                uMax = u;
+            }
+        }
+        printf("%d\n", ++uMax);
+    }
+    return 0;
+}
