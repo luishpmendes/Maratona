b0e7728971a0d7d1c32c169d5fbee26e0da960fe
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-01-22 14:32:23 -0200

    URI - 1013 : The Greatest

--- uri/1013/greatest.py
+++ uri/1013/greatest.py
@@ -3,9 +3,6 @@ abc = abc.split(' ')
 a = int(abc[0])
 b = int(abc[1])
 c = int(abc[2])
-maior = a;
-if maior < b :
-    maior = b
-if maior < c :
-    maior = c
+maiorab = (a + b + abs(a - b)) / 2;
+maior = (maiorab + c + abs(maiorab - c)) / 2;
 print(str(maior) + ' eh o maior')
