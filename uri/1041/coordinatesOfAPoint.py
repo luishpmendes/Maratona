7218512c37e8fb6355a80d73d3e1876b91198bef
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-01-28 15:09:34 -0200

    URI - 1041 : Coordinates of a Point

---
+++ uri/1041/coordinatesOfAPoint.py
@@ -0,0 +1,18 @@
+line = raw_input()
+line = line.split(' ')
+x = float(line[0])
+y = float(line[1])
+if x == 0.0 and y == 0.0 :
+    print('Origem')
+elif x == 0.0 :
+    print('Eixo Y')
+elif y == 0.0 :
+    print('Eixo X')
+elif x > 0.0 and y > 0.0 :
+    print('Q1')
+elif x < 0.0 and y > 0.0 :
+    print('Q2')
+elif x < 0.0 and y < 0.0 :
+    print('Q3')
+else :
+    print('Q4')
