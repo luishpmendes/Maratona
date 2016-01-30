ae7cc113bd147fd0be2f7ed2a3bce32391ea2f7d
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-01-30 18:54:20 -0200

    URI - 1024 : Encryption

---
+++ uri/1024/encryption.py
@@ -0,0 +1,12 @@
+while True :
+    n = int(raw_input())
+    for _ in range(n) :
+        s = raw_input()
+        l = list(s)
+        for i in range(len(l)) :
+            if (l[i] >= 'A' and l[i] <= 'Z') or (l[i] >= 'a' and l[i] <= 'z') :
+                l[i] = chr(ord(l[i]) + 3)
+        l.reverse()
+        for i in range(len(l)/2, len(l)) :
+            l[i] = chr(ord(l[i]) - 1)
+        print(''.join(l))
